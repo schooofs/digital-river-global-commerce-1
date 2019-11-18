bb88132337dc325fbf7846da47179e7788efa316
Tracy Wang <twang@digitalriver.com>
2019-11-18 14:23:12 +0800

    remove site config information

--- tests/Testcafe/siteConfig.js
+++ tests/Testcafe/siteConfig.js
@@ -1,10 +1,10 @@
 let siteConfig = {
   siteDrdod15: {
-    siteID:     'drdod15',
-    apiKey:     '99477953970e432da4d89b982f6bcc49',
-    apiSecret:  'a4dccc3558ec4e09ae2879864f900f24',
-    domainInput:  'api.digitalriver.com',
-    pluginKey:  '6eb251a648bc4e6b87b24671262f2e91',
+    siteID:     'SITE_ID',
+    apiKey:     'API_KEY',
+    apiSecret:  'API_SECRET',
+    domainInput:  'DOMAIN_INPUT',
+    pluginKey:  'PLUGIN_KEY',
   }
 };
 

25e4fce0221a0e47d92d6dda6661b1dee5f04fde
Edison Wu <ewu@digitalriver.com>
2019-11-21 19:06:41 +0800

    check if the shipping is required for the source event

--- assets/js/public/payment-googlepay.js
+++ assets/js/public/payment-googlepay.js
@@ -2,7 +2,7 @@ import CheckoutUtils from './checkout-utils';
 import DRCommerceApi from './commerce-api';
 
 const DRGooglePay = (($, translations) => {
-  const initGooglePayEvents = (googlepay) => {
+  const initGooglePayEvents = (googlepay, requestShipping) => {
     googlepay.on('shippingaddresschange', (event) => {
       const shippingAddress = event.shippingAddress;
 
@@ -92,25 +92,46 @@ const DRGooglePay = (($, translations) => {
     });
 
     googlepay.on('source', (event) => {
-      const sourceId = event.source.id;      
-      const shippingAddressObj = {
-        id: 'shippingAddress',
-        firstName: event.shippingAddress.firstName,
-        lastName: event.shippingAddress.lastName,
-        line1: event.shippingAddress.address.line1,
-        line2: event.shippingAddress.address.line2,
-        city: event.shippingAddress.address.city,
-        countrySubdivision: event.shippingAddress.address.state,
-        postalCode: event.shippingAddress.address.postalCode,
-        country: event.shippingAddress.address.country,
-        phoneNumber: event.shippingAddress.phone,
-        emailAddress: event.shippingAddress.email
+      const cartRequest = {};
+      const sourceId = event.source.id;
+      const billingAddressObj = {
+        id: 'billingAddress',
+        firstName: event.billingAddress.firstName,
+        lastName: event.billingAddress.lastName,
+        line1: event.billingAddress.address.line1,
+        line2: event.billingAddress.address.line2,
+        city: event.billingAddress.address.city,
+        countrySubdivision: event.billingAddress.address.state,
+        postalCode: event.billingAddress.address.postalCode,
+        country: event.billingAddress.address.country,
+        phoneNumber: event.billingAddress.phone,
+        emailAddress: event.billingAddress.email
       };
+
+      cartRequest.billingAddress = billingAddressObj;
+
+      if (requestShipping) {
+        const shippingAddressObj = {
+          id: 'shippingAddress',
+          firstName: event.shippingAddress.firstName,
+          lastName: event.shippingAddress.lastName,
+          line1: event.shippingAddress.address.line1,
+          line2: event.shippingAddress.address.line2,
+          city: event.shippingAddress.address.city,
+          countrySubdivision: event.shippingAddress.address.state,
+          postalCode: event.shippingAddress.address.postalCode,
+          country: event.shippingAddress.address.country,
+          phoneNumber: event.shippingAddress.phone,
+          emailAddress: event.shippingAddress.email
+        };
+
+        cartRequest.shippingAddress = shippingAddressObj;
+      }
 
       sessionStorage.setItem('paymentSourceId', sourceId);
       $('body').css({'pointer-events': 'none', 'opacity': 0.5});
 
-      DRCommerceApi.updateCart({expand: 'all'}, {shippingAddress: shippingAddressObj}).then((data) => {
+      DRCommerceApi.updateCart({expand: 'all'}, cartRequest).then((data) => {
         DRCommerceApi.applyPaymentAndSubmitCart(sourceId);
       });
 
@@ -134,7 +155,7 @@ const DRGooglePay = (($, translations) => {
     const googlepay = digitalriverJs.createElement('googlepay', googlePayRequestData);
 
     if (googlepay.canMakePayment()) {
-      initGooglePayEvents(googlepay);
+      initGooglePayEvents(googlepay, requestShipping);
       googlepay.mount('dr-googlepay-button');
       document.getElementById('dr-googlepay-button').style.border = 'none';
 

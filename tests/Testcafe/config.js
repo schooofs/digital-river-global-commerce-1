31ff3642e82597c8cb6e259d30025dac71906619
Tracy Wang <twang@digitalriver.com>
2019-12-23 17:50:13 +0800

    remote config settings

--- tests/Testcafe/config.js
+++ tests/Testcafe/config.js
@@ -1,32 +1,32 @@
 let config = {
-  env: 'dev', // local: dev; staging/systest: sys; production/demo: prod
+  env: 'env', // local: dev; staging/systest: sys; production/demo: prod
   apiEnv: 'PRD',
   testEmail: 'qa@dr.com',
   adminUserName: {
-    dev: 'tracy', //Replace this to your localhost admin username
+    dev: 'username', //Replace this to your localhost admin username
     sys: 'username',
     prod: 'username',
     demo: 'username',
   },
   adminPassword: {
-    dev: 'tracyadmin', //Replace this to your localhost admin password
+    dev: 'password', //Replace this to your localhost admin password
     sys: 'password',
     prod: 'password',
     demo: 'password',
   },
   baseUrl: {
-    dev: 'http://10.70.10.35/wordpress/',  //Replace this to your localhost test site address
-    sys: 'http://tpedevapp0264.d010.digitalriverws.net/',
-    prod: 'http://wordpress.c141.digitalriverws.net/',
-    demo: 'http://gcwpdemo.wpengine.com/',
+    dev: 'Dev Site Web Address',  //Replace this to your localhost test site address
+    sys: 'Systest Site Web Address',
+    prod: 'Prod Site Web Address',
+    demo: 'Demo Site Web Address',
   },
   apiUrl: {
-    PRD: 'https://api.digitalriver.com',
-    NONPRD: 'https://dispatch-test.digitalriver.com',
+    PRD: 'Prod API URL',
+    NONPRD: 'NonProd API URL',
   },
   websiteAuth: {
-    username: 'gcwpdemo',
-    password: '33a5b9f5',
+	username: 'username',
+	password: 'password'
   }
 };
 

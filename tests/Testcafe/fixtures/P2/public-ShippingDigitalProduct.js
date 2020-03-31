cecaa2597038d07c8c0c2f49032cdd8f610197f3
Tracy Wang <twang@digitalriver.com>
2020-03-31 09:18:37 +0800

    using isVariation flag instead

--- tests/Testcafe/fixtures/P2/public-ShippingDigitalProduct.js
+++ tests/Testcafe/fixtures/P2/public-ShippingDigitalProduct.js
@@ -34,7 +34,7 @@ test('Estimated Shipping Fee - Standard free Shipping', async t => {
   // Add a physical product into cart
   console.log('>> Add Digital product into cart');
   await t.setTestSpeed(0.9);
-  await utils.addProductsIntoCart(homePage.addDigiProduct, 'digital');
+  await utils.addProductsIntoCart(homePage.addDigiProduct, true);
   await t.takeScreenshot('BWC/minicart.jpg');
 
   // Click View Cart btn in miniCart to go to Cart page
